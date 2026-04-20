ec6788b6a8c253480df95e7b2801586c1990d54b
KhilanKanani <khilankanani50@gmail.com>
2026-04-20 14:28:26 +0530

    All

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from './App.jsx'
 import { Toaster } from 'sonner'
 
 export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
